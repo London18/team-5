a0201dca12b7c7422a043ac223f011206d1ffefa
Liam Pugh <liampugh@limelight.tech>
2018-11-09 23:50:15 +0000

    Email Handling #InsertEmailPun

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -1,6 +1,12 @@
+import mail.MailHandler;
+
 public class Tests {
 
     public static void main(String[] args) {
         System.out.println(DatabaseHelper.instance().getConnection());
+		MailHandler mailHandler = new MailHandler();
+		if(mailHandler.sendEmail("liampugh@limelight.tech","Test Subject","Test Body") == 0){
+			System.out.println("Email test successful");
+		}
     }
 }
---
+++ src/main/java/mail/MailHandler.java
@@ -0,0 +1,69 @@
+package mail;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class MailHandler {
+
+	final String fromEmail = "Code4GoodTeam5@gmail.com"; //requires valid gmail id
+	final String password = "ELiXiRC4GT5"; // correct password for gmail id
+
+
+	public MailHandler(){
+
+	}
+
+	public int sendEmail(String toEmail, String subject, String body){
+		try {
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+			props.put("mail.smtp.port", "587"); //TLS Port
+			props.put("mail.smtp.auth", "true"); //enable authentication
+			props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+			//create Authenticator object to pass in Session.getInstance argument
+			Authenticator auth = new Authenticator() {
+				//override the getPasswordAuthentication method
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(fromEmail, password);
+				}
+			};
+			Session session = Session.getInstance(props, auth);
+			MimeMessage msg = new MimeMessage(session);
+			//set message headers
+			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+			msg.addHeader("format", "flowed");
+			msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+			msg.setFrom(new InternetAddress("Code4GoodTeam5@gmail.com", "Team Elixir"));
+
+			msg.setReplyTo(InternetAddress.parse("Code4GoodTeam5@gmail.com", false));
+
+			msg.setSubject(subject, "UTF-8");
+
+			msg.setText(body, "UTF-8");
+
+			msg.setSentDate(new Date());
+
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+			Transport.send(msg);
+
+		} catch (Exception e) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
