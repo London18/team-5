4a1461f15c61979eebcfc5f26808dcbff7b8a459
Liam Pugh <liampugh@limelight.tech>
2018-11-10 01:37:16 +0000

    Password security fix, opening for DB fetching methods... #AlreadySleepy

--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -1,8 +1,15 @@
 import auth.AuthenticationHelper;
+import mail.MailHandler;
 
 public class Tests {
 
     public static void main(String[] args) throws Exception {
         System.out.println(AuthenticationHelper.authenticate("tobichammings", "tobipass").getState());
+        MailHandler mailHandler = new MailHandler();
+        if(mailHandler.sendEmail("liampugh@limelight.tech","Test","Body") == 0){
+        	System.out.println("Email sent successfully");
+		}else{
+			System.out.println("Email FAILED");
+		}
     }
 }
