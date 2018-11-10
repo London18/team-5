d4af7a8843e6a761c72d398fd52781cd6c627104
Liam Pugh <liampugh@limelight.tech>
2018-11-10 02:48:04 +0000

    Session Fetcher Refactoring for Collaborative code

--- src/main/java/db/sessionFetcher.java
+++ src/main/java/db/sessionFetcher.java
@@ -1,8 +1,11 @@
 package db;
 
-import com.mysql.cj.protocol.Resultset;
+import data.sessionInformation;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class sessionFetcher {
@@ -16,16 +19,36 @@ public class sessionFetcher {
 	}
 
 
-	public String[] getSessionInfo(String user){
-		String[] toReturn = new String[4];
-		Date date = new Date();
+	public sessionInformation getSessionInfo(String user){
+		LocalDateTime localDateTime = LocalDateTime.now();
 
 		PreparedStatement preparedStatement = null;
 
-		Resultset results;
+		ResultSet results;
+
+		// Do they have overdue safe place request?
+		try {
+			preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, SIT.STARTTD, SIT.ENDTD"
+					+ " FROM STAFF, SESSIONS SIT, SESSIONSASS ASS, SES"
+					+ "SIONENDSTATE END, HOMESTATUS HOME, PASSWORDS PASS WHERE SIT.ENDTD = ASS.ENDTD AND STAFF.PAYROLLN"
+					+ "UM =ASS.PAYROLLNUM AND HOME.PAYROLLNUM = STAFF.PAYROLLNUM AND ASS.ENDTD > NOW() AND END.PARTNERI"
+					+ "D =ASS.PARTNERID AND HOME.HOME = 0 AND PASS.USERNAME = ? AND PASS.PAYROLLNUM = STAFF.PAYROLLNU"
+					+ "M ORDER BY SIT.STARTTD ASC;");
+			preparedStatement.setString(0, user);
+			results = preparedStatement.executeQuery();
+			if(results.next()){
+				return new sessionInformation(1, results.getString("STARTTD"),
+						results.getString("ENDTD"),results.getString("NAME"));
+			}else{
+				// No overdue safe place requests, so any overdue left patient requests?
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 
-		return toReturn;
+		return new sessionInformation(0,null,null,"Liam");
 
 	}
 
