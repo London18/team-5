be9c05719bcd059d729d82e476992dfaebaf19fe
Liam Pugh <liampugh@limelight.tech>
2018-11-10 03:10:31 +0000

    Fetcher for sessions complete

--- src/main/java/db/sessionFetcher.java
+++ src/main/java/db/sessionFetcher.java
@@ -12,14 +12,16 @@ public class sessionFetcher {
 
 	private DatabaseHelper db;
 
-	public sessionFetcher(){
+
+	public sessionFetcher() {
 		try {
 			this.db = DatabaseHelper.instance();
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
 
 
-	public sessionInformation getSessionInfo(String user){
+	public sessionInformation getSessionInfo(String user) {
 		LocalDateTime localDateTime = LocalDateTime.now();
 
 		PreparedStatement preparedStatement = null;
@@ -28,28 +30,70 @@ public class sessionFetcher {
 
 		// Do they have overdue safe place request?
 		try {
-			preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, SIT.STARTTD, SIT.ENDTD"
-					+ " FROM STAFF, SESSIONS SIT, SESSIONSASS ASS, SES"
-					+ "SIONENDSTATE END, HOMESTATUS HOME, PASSWORDS PASS WHERE SIT.ENDTD = ASS.ENDTD AND STAFF.PAYROLLN"
-					+ "UM =ASS.PAYROLLNUM AND HOME.PAYROLLNUM = STAFF.PAYROLLNUM AND ASS.ENDTD > NOW() AND END.PARTNERI"
-					+ "D =ASS.PARTNERID AND HOME.HOME = 0 AND PASS.USERNAME = ? AND PASS.PAYROLLNUM = STAFF.PAYROLLNU"
-					+ "M ORDER BY SIT.STARTTD ASC;");
-			preparedStatement.setString(0, user);
+			preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, STARTTD, ENDTD FR" +
+					"OM STAFF, SESSIONS, SESSIONSASS, PASSWORDS PASS WHERE STAFF.PAYROLLNUM = PASS.PAYROLLNUM AND " +
+					"SESSIONSASS.PAYROLLNUM = STAFF.PAYROLLNUM AND SESSIONS.SESSIONID = SESSIONSASS.SESSIONID AND " +
+					"SESSIONSASS.LEFTHOME = 1 AND SESSIONSASS.HOME = 0 AND PASS.USERNAME = ? ORDER BY STARTTD ASC;");
+			preparedStatement.setString('0',user);
 			results = preparedStatement.executeQuery();
-			if(results.next()){
+			if (results.next()) {
 				return new sessionInformation(1, results.getString("STARTTD"),
-						results.getString("ENDTD"),results.getString("NAME"));
-			}else{
+						results.getString("ENDTD"), results.getString("NAME"));
+			} else {
 				// No overdue safe place requests, so any overdue left patient requests?
-
+				preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, STARTTD, E" +
+						"NDTD FROM STAFF, SESSIONS, SESSIONSASS, PASSWORDS PASS WHERE STAFF.PAYROLLNUM = PASS.PA" +
+						"YROLLNUM AND SESSIONSASS.PAYROLLNUM = STAFF.PAYROLLNUM AND SESSIONS.SESSIONID = SESSIONS" +
+						"ASS.SESSIONID AND SESSIONSASS.LEFTHOME = 0 AND SESSIONSASS.HOME = 0 AND SESSIONS.ENDTD <" +
+						" NOW() AND PASS.USERNAME = ? ORDER BY STARTTD ASC;");
+				preparedStatement.setString('0',user);
+				results = preparedStatement.executeQuery();
+				if (results.next()) {
+					return new sessionInformation(1, results.getString("STARTTD"),
+							results.getString("ENDTD"), results.getString("NAME"));
+				} else {
+					// No overdue left home requests, so any current requests?
+					preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, STARTTD, E" +
+							"NDTD FROM STAFF, SESSIONS, SESSIONSASS, PASSWORDS PASS WHERE STAFF.PAYROLLNUM = PASS.PA" +
+							"YROLLNUM AND SESSIONSASS.PAYROLLNUM = STAFF.PAYROLLNUM AND SESSIONS.SESSIONID = SESSIONS" +
+							"ASS.SESSIONID AND SESSIONSASS.LEFTHOME = 0 AND SESSIONSASS.HOME = 0 AND SESSIONS.STARTTD" +
+							" < NOW() AND PASS.USERNAME = ? ORDER BY STARTTD ASC;");
+					preparedStatement.setString('0',user);
+					results = preparedStatement.executeQuery();
+					if (results.next()) {
+						return new sessionInformation(1, results.getString("STARTTD"),
+								results.getString("ENDTD"), results.getString("NAME"));
+					} else {
+						//No current requests, any future requests
+						preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME, STARTTD" +
+								", ENDTD FROM STAFF, SESSIONS, SESSIONSASS, PASSWORDS PASS WHERE STAFF.PAYROLLNUM = " +
+								"PASS.PAYROLLNUM AND SESSIONSASS.PAYROLLNUM = STAFF.PAYROLLNUM AND SESSIONS.SESSIONID " +
+								"= SESSIONSASS.SESSIONID AND SESSIONSASS.LEFTHOME = 0 AND SESSIONSASS.HOME = 0 AND" +
+								" SESSIONS.STARTTD > NOW() AND PASS.USERNAME = ? ORDER BY STARTTD ASC;");
+						preparedStatement.setString('0',user);
+						results = preparedStatement.executeQuery();
+						if (results.next()) {
+							return new sessionInformation(1, results.getString("STARTTD"),
+									results.getString("ENDTD"), results.getString("NAME"));
+						} else {
+							//No future requests
+							preparedStatement = this.db.getConnection().prepareStatement("SELECT STAFF.NAME FRO" +
+									"M PASSWORDS, STAFF WHERE PASSWORDS.PAYROLLNUM = STAFF.PAYROLLNUM AND USERNAME " +
+									"= ?;");
+							preparedStatement.setString('0',user);
+							results = preparedStatement.executeQuery();
+							results.next();
+							return new sessionInformation(2,null,null,
+									results.getString("NAME"));
+						}
+					}
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
-
-		return new sessionInformation(0,null,null,"Liam");
-
+		return new sessionInformation(2,null,null,"N/A");
 	}
 
 
